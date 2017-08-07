38db139728633e5e04e599ac891f12db0ea98a36
yanglin <richenlin@gmail.com>
2017-08-07 18:04:48 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# think_cookie
+# 介绍
+-----
+
+[![npm version](https://badge.fury.io/js/think_cookie.svg)](https://badge.fury.io/js/think_cookie)
+[![Dependency Status](https://david-dm.org/thinkkoa/think_cookie.svg)](https://david-dm.org/thinkkoa/think_cookie)
+
 Cookie for ThinkKoa.
+
+# 安装
+-----
+
+```
+npm i think_cookie
+```
+
+# 使用
+-----
+
+1、cookie中间件为thinkkoa内置中间件,无需在项目中创建引用。该中间件默认开启
+
+2、项目中间件配置 config/middleware.js:
+```
+config: { //中间件配置
+    ...,
+    cookie: {
+        domain: '',  // cookie所在的域名
+        path: '/',       // cookie所在的路径
+        maxAge: -1, // cookie有效时长
+        httpOnly: true,  // 是否只用于http请求中获取
+        overwrite: false,  // 是否允许重写
+        //expires: new Date('2017-02-15')  // cookie失效时间
+    }
+}
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+/**
+ *
+ * @author     richen
+ * @copyright  Copyright (c) 2017 - <richenlin(at)gmail.com>
+ * @license    MIT
+ * @version    17/4/29
+ */
+const lib = require('think_lib');
+
+/**
+ * default options
+ */
+const defaultOptions = {
+    domain: '',  // cookie所在的域名
+    path: '/',       // cookie所在的路径
+    maxAge: -1, // cookie有效时长
+    httpOnly: true,  // 是否只用于http请求中获取
+    overwrite: false,  // 是否允许重写
+    //expires: new Date('2017-02-15')  // cookie失效时间
+};
+
+module.exports = function (options) {
+    options = options ? lib.extend(defaultOptions, options, true) : defaultOptions;
+    return function (ctx, next) {
+        /**
+         * get or set cookie
+         * @param {String} name
+         * @param {String} value
+         * @param {Object} options
+         */
+        lib.define(ctx, 'cookie', function (name, value, option = {}) {
+            if (!lib.isString(name)) {
+                ctx.throw('cookie.name must be a string');
+                return null;
+            }
+            //get cookie
+            if (value === undefined) {
+                return ctx.cookies.get(name, option);
+            }
+            option = lib.extend(options, option || {});
+            //remove cookie
+            if (value === null) {
+                return ctx.cookies.set(name, '', {
+                    maxAge: -1
+                });
+            }
+            if (!lib.isString(value)) {
+                ctx.throw('cookie value must be a string');
+                return null;
+            }
+            //http://browsercookielimits.squawky.net/
+            if (value.length >= 4094) {
+                ctx.throw('cookie limit has error length');
+                return null;
+            }
+            //set cookie
+            return ctx.cookies.set(name, value, option);
+        });
+
+        return next();
+    };
+};
+
